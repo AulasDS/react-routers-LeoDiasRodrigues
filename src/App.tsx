c82d0783cada817e7dbf90d1d23efc6d3b723ae3
LeoDiasRodrigues <leozezudr@gmail.com>
2026-04-29 17:12:10 -0300

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/home'
+import Products from './pages/products'
+import { Navbar } from 'react-bootstrap'
+
+function App() {
+
+  return (
+    <>
+      <Routes>
+        <Route path='/' element={<Home/>}></Route>
+        <Route path="/products" element={<Products/>}></Route>
+      </Routes>
+    </>
+  )
+}
+
+export default App
